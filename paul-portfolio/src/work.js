12bccbc4e39096f24a11d880a74c3bf52568cba2
Paul Sherrill <pauldsherrill@yahoo.com>
2024-07-15 22:01:22 -0500

    started changing images to js

--- paul-portfolio/src/work.js
+++ paul-portfolio/src/work.js
@@ -1,140 +1,157 @@
+import symantic2 from "./assets/horiseon2.png";
+import portfolio1 from "./assets/portfolio1.png";
+import employeeTracker1 from "./assets/employeetracker1.png";
+import personalBlog1 from "./assets/blog1-1.png";
+import taskboard1 from "./assets/taskboard1.png";
+import fitness1 from "./assets/fitness-tracker1.png";
+import weather2 from "./assets/weather-dashboard2.png";
+import logo1 from "./assets/logo-generator1.png";
+import readme1 from "./assets/readme-generator.png";
+import noteTaker1 from "./assets/note-taker1.png";
+import employeedb1 from "./assets/employeedb.png";
+import eCommerce1 from "./assets/e-commerce1.png";
+import bootcamp1 from "./assets/bootcamp-banking1.png";
+import techBlog1 from "./assets/blog2-1.png";
+import socialNetwork1 from "./assets/social-network1.png";
+import placeholder from "./assets/placeholder.png";
+
 const projects = [
   {
     href: "repos#symantic-html",
-    src: "./assets/horiseon2.png",
+    src: symantic2,
     alt: "SYMANTIC HTML",
     classes: "img-fluid html",
     id: 1,
   },
   {
     href: "repos#portfolio",
-    src: "./assets/portfolio1.png",
+    src: portfolio1,
     alt: "BEGINNER PORTFOLIO",
     classes: "img-fluid html css",
     id: 2,
   },
   {
     href: "repos#payroll",
-    src: "./assets/employeetracker1.png",
+    src: employeeTracker1,
     alt: "EMPLOYEE PAYROLL TRACKER",
     classes: "img-fluid html js",
     id: 3,
   },
   {
     href: "repos#personal-blog",
-    src: "./assets/blog1-1.png",
+    src: personalBlog1,
     alt: "PERSONAL BLOG",
     classes: "img-fluid html css js",
     id: 4,
   },
   {
     href: "repos#taskboard",
-    src: "./assets/taskboard1.png",
+    src: taskboard1,
     alt: "TASKBOARD",
     classes: "img-fluid js bs jq html",
     id: 5,
   },
   {
     href: "repos#fitness",
-    src: "./assets/fitness-tracker1.png",
+    src: fitness1,
     alt: "HEALTH AND FITNESS TRACKER",
     classes: "img-fluid html js tw",
     id: 6,
   },
   {
     href: "repos#weather",
-    src: "./assets/weather-dashboard2.png",
+    src: weather2,
     alt: "WEATHER DASHBOARD",
     classes: "img-fluid html js bs",
     id: 7,
   },
   {
     href: "repos#logo",
-    src: "./assets/logo-generator1.png",
+    src: logo1,
     alt: "LOGO GENERATOR",
     classes: "img-fluid js node",
     id: 8,
   },
   {
     href: "repos#readme",
-    src: "./assets/readme-generator.png",
+    src: readme1,
     alt: "README GENERATOR",
     classes: "img-fluid js node",
     id: 9,
   },
   {
     href: "repos#note-taker",
-    src: "./assets/note-taker1.png",
+    src: noteTaker1,
     alt: "NOTE TAKER",
     classes: "img-fluid html css bs express js node",
     id: 10,
   },
   {
     href: "repos#employee",
-    src: "./assets/employeedb.png",
+    src: employeedb1,
     alt: "EMPLOYEE DATABASE",
     classes: "img-fluid js node insomnia express pg",
     id: 11,
   },
   {
     href: "repos#e-commerce",
-    src: "./assets/e-commerce1.png",
+    src: eCommerce1,
     alt: "E-COMMERCE BACKEND",
     classes: "img-fluid js node insomnia express pg",
     id: 12,
   },
   {
     href: "repos#bootcamp",
-    src: "./assets/bootcamp-banking1.png",
+    src: bootcamp1,
     alt: "BOOTCAMP BANKING",
     classes: "img-fluid css js jq bs insomnia node express pg hb html",
     id: 13,
   },
   {
     href: "repos#tech-blog",
-    src: "./assets/blog2-1.png",
+    src: techBlog1,
     alt: "TECH BLOG",
     classes: "img-fluid html css js jq bs insomnia node express pg hb",
     id: 14,
   },
   {
     href: "repos#social-network",
-    src: "./assets/social-network1.png",
+    src: socialNetwork1,
     alt: "SOCIAL NETWORK API",
     classes: "img-fluid js insomnia node mdb",
     id: 15,
   },
   {
     href: "repos.html#payroll-tracker",
-    src: "./assets/placeholder.png",
+    src: placeholder,
     alt: "PROJECT IN PROGRESS",
     classes: "img-fluid",
     id: 16,
   },
   {
     href: "repos.html#payroll-tracker",
-    src: "./assets/placeholder.png",
+    src: placeholder,
     alt: "PROJECT IN PROGRESS",
     classes: "img-fluid",
     id: 17,
   },
   {
     href: "repos.html#payroll-tracker",
-    src: "./assets/placeholder.png",
+    src: placeholder,
     alt: "PROJECT IN PROGRESS",
     classes: "img-fluid",
     id: 18,
   },
   {
     href: "repos.html#payroll-tracker",
-    src: "./assets/placeholder.png",
+    src: placeholder,
     alt: "PROJECT IN PROGRESS",
     classes: "img-fluid",
     id: 19,
   },
   {
     href: "repos.html#payroll-tracker",
-    src: "./assets/placeholder.png",
+    src: placeholder,
     alt: "PROJECT IN PROGRESS",
     classes: "img-fluid",
     id: 20,
